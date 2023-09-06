c13adc786b7ca89555b100ed4a73be75e66f2b93
AQIBMUGHAL1 <fa21-bcs-073@cuilahore.edu.pk>
2023-09-06 10:24:39 +0500

    🚧 component added

--- src/Components/Generate.jsx
+++ src/Components/Generate.jsx
@@ -8,16 +8,26 @@ const Generate = () => {
     <>
       <div class="container mx-auto bg-black">
         <div class="grid grid-cols-3 gap-20 mx-32">
-          <div class="col-span-1 bg-gray-200 p-4">
-            <img src={img21} alt='image21' className="h-16" />
-            
+          <div class="col-span-1 bg-black p-4 border-t-2">
+            <img src={img21} alt='image21' className="h-16 " />
+            <h1 className="text-lg font-bold text-white py-5" >Generate designs in a seconds</h1>
+            <p className="text-sm text-white mr-10">Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+              Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.</p>
           </div>
-          <div class="col-span-1 bg-gray-200 p-4">
+          <div class="col-span-1 bg-black p-4 border-t-2">
             <img src={img22} alt='image22' className="h-16" />
+            <h1 className="text-lg font-bold text-white py-5 ">Highly-tuned tools</h1>
+            <p className="text-sm text-white mr-10">Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+              Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.</p>
+          
             
           </div>
-          <div class="col-span-1 bg-gray-200 p-4">
+          <div class="col-span-1 bg-black p-4 border-t-2">
             <img src={img23} alt='image23' className="h-16" />
+            <h1 className="text-lg font-bold text-white py-5 ">Enter your projects </h1>
+            <p className="text-sm text-white mr-10">Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+              Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.</p>
+          
 
           </div>
         </div>
