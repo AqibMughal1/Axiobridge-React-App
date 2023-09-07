40159554cee064ee75106cd9e3617cd70248125a
AQIBMUGHAL1 <fa21-bcs-073@cuilahore.edu.pk>
2023-09-07 18:54:03 +0500

    ✨

--- src/Components/Toolcard.jsx
+++ src/Components/Toolcard.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import img28 from '../Assets/Group 40.png'
+import { AiFillStar } from 'react-icons/ai'
 
 const Toolcard = () => {
     return (
@@ -8,10 +9,19 @@ const Toolcard = () => {
                 <div className="bg-[#181725] md:mx-72 rounded-3xl flex-col md:flex md:flex-row">
                     <div className="p-14">
                         <img src={img28} alt='image28' className="md:h-96 " />
-                    </div>
-                    <div className="md:border-l-2 my-12">
-                        <div className="ml-10">
-                            <h1 className="text-white">Axiobridge ceative tools you need in <span className="text-[#1F70AF]">one platform</span></h1>
+                    </div >
+                    <div className="md:border-l-2 my-12 w-1/2">
+                        <div className="ml-20 flex flex-col">
+                            <div>
+                                <h1 className="text-white text-lg md:leading-8 font-bold pr-32 ">Axiobridge ceative tools you need in
+                                    <span className="text-[#1F70AF]"> one platform</span></h1>
+                            </div>
+                            {/* <AiFillStar className='text-white'/> */}
+
+                            <p className="text-white text-sm leading-7 flex-grow-0 pr-20 py-6">Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                                Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.</p>
+                            
+                            
 
                         </div>
                     </div>
