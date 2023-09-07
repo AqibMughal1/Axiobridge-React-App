d9216ff34acda720dddc3f42656a8a76240a6fc2
TALHA CH <talhashafiqch@gmail.com>
2023-09-07 19:31:24 +0500

    first-commit

--- src/Components/Services.jsx
+++ src/Components/Services.jsx
@@ -1,5 +1,10 @@
 import React from "react";
-
+import Ellipsea1 from "../Assets/Ellipse a1.png";
+import Ellipsea2 from "../Assets/Ellipse a2.png";
+import Arrow1 from "../Assets/Arrow 1.png";
+import Arrow2 from "../Assets/Arrow 2.png";
+import img1 from "../Assets/img bg-1.png";
+import img2 from "../Assets/img bg-2.png";
 const Services = () => {
   return (
     <div className="bg-[#181725] text-white text-center py-12 sm:py-24">
@@ -10,16 +15,209 @@ const Services = () => {
         Solutions
         <span className="text-[#1F70AF]"> Platform</span>
       </h1>
-      <div>
-        <div>
-          <div></div>
-          <div></div>
-          <div></div>
+      <div className="grid grid-cols-2 place-items-center mx-28 mt-36">
+        {/* card no 1 */}
+        <div className="flex bg-black rounded-xl justify-center items-center p-8 w-[90%]">
+          <div>
+            <img src={img1} alt="" />
+          </div>
+          <div className="text-left px-12">
+            <h1>Graphic Designing</h1>
+            <p>
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry.
+            </p>
+          </div>
+
+          <div className="relative">
+            <img src={Ellipsea1} alt="" />
+            <div className="absolute top-2 left-1">
+              <img src={Arrow1} alt="" />
+            </div>
+          </div>
         </div>
-        <div>
-          <div></div>
-          <div></div>
-          <div></div>
+        {/* card no 2 */}
+        <div className="flex bg-black rounded-xl justify-center items-center p-8 w-[90%]">
+          <div>
+            <img src={img2} alt="" />
+          </div>
+          <div className="text-left px-12">
+            <h1>Web & App Development</h1>
+            <p>
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry.
+            </p>
+          </div>
+          <div className="relative">
+            <img src={Ellipsea2} alt="" />
+            <div className="absolute top-2 left-1">
+              <img src={Arrow2} alt="" />
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="grid grid-cols-2 place-items-center mx-28 mt-8">
+        {/* card no 1 */}
+        <div className="flex bg-black rounded-xl justify-center items-center p-8 w-[90%]">
+          <div>
+            <img src={img1} alt="" />
+          </div>
+          <div className="text-left px-12">
+            <h1>Graphic Designing</h1>
+            <p>
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry.
+            </p>
+          </div>
+
+          <div className="relative">
+            <img src={Ellipsea1} alt="" />
+            <div className="absolute top-2 left-1">
+              <img src={Arrow1} alt="" />
+            </div>
+          </div>
+        </div>
+        {/* card no 2 */}
+        <div className="flex bg-black rounded-xl justify-center items-center p-8 w-[90%]">
+          <div>
+            <img src={img2} alt="" />
+          </div>
+          <div className="text-left px-12">
+            <h1>Web & App Development</h1>
+            <p>
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry.
+            </p>
+          </div>
+          <div className="relative">
+            <img src={Ellipsea2} alt="" />
+            <div className="absolute top-2 left-1">
+              <img src={Arrow2} alt="" />
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="grid grid-cols-2 place-items-center mx-28 mt-8">
+        {/* card no 1 */}
+        <div className="flex bg-black rounded-xl justify-center items-center p-8 w-[90%]">
+          <div>
+            <img src={img1} alt="" />
+          </div>
+          <div className="text-left px-12">
+            <h1>Graphic Designing</h1>
+            <p>
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry.
+            </p>
+          </div>
+
+          <div className="relative">
+            <img src={Ellipsea1} alt="" />
+            <div className="absolute top-2 left-1">
+              <img src={Arrow1} alt="" />
+            </div>
+          </div>
+        </div>
+        {/* card no 2 */}
+        <div className="flex bg-black rounded-xl justify-center items-center p-8 w-[90%]">
+          <div>
+            <img src={img2} alt="" />
+          </div>
+          <div className="text-left px-12">
+            <h1>Web & App Development</h1>
+            <p>
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry.
+            </p>
+          </div>
+          <div className="relative">
+            <img src={Ellipsea2} alt="" />
+            <div className="absolute top-2 left-1">
+              <img src={Arrow2} alt="" />
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="grid grid-cols-2 place-items-center mx-28 mt-8">
+        {/* card no 1 */}
+        <div className="flex bg-black rounded-xl justify-center items-center p-8 w-[90%]">
+          <div>
+            <img src={img1} alt="" />
+          </div>
+          <div className="text-left px-12">
+            <h1>Graphic Designing</h1>
+            <p>
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry.
+            </p>
+          </div>
+
+          <div className="relative">
+            <img src={Ellipsea1} alt="" />
+            <div className="absolute top-2 left-1">
+              <img src={Arrow1} alt="" />
+            </div>
+          </div>
+        </div>
+        {/* card no 2 */}
+        <div className="flex bg-black rounded-xl justify-center items-center p-8 w-[90%]">
+          <div>
+            <img src={img2} alt="" />
+          </div>
+          <div className="text-left px-12">
+            <h1>Web & App Development</h1>
+            <p>
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry.
+            </p>
+          </div>
+          <div className="relative">
+            <img src={Ellipsea2} alt="" />
+            <div className="absolute top-2 left-1">
+              <img src={Arrow2} alt="" />
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="grid grid-cols-2 place-items-center mx-28 mt-8">
+        {/* card no 1 */}
+        <div className="flex bg-black rounded-xl justify-center items-center p-8 w-[90%]">
+          <div>
+            <img src={img1} alt="" />
+          </div>
+          <div className="text-left px-12">
+            <h1>Graphic Designing</h1>
+            <p>
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry.
+            </p>
+          </div>
+
+          <div className="relative">
+            <img src={Ellipsea1} alt="" />
+            <div className="absolute top-2 left-1">
+              <img src={Arrow1} alt="" />
+            </div>
+          </div>
+        </div>
+        {/* card no 2 */}
+        <div className="flex bg-black rounded-xl justify-center items-center p-8 w-[90%]">
+          <div>
+            <img src={img2} alt="" />
+          </div>
+          <div className="text-left px-12">
+            <h1>Web & App Development</h1>
+            <p>
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry.
+            </p>
+          </div>
+          <div className="relative">
+            <img src={Ellipsea2} alt="" />
+            <div className="absolute top-2 left-1">
+              <img src={Arrow2} alt="" />
+            </div>
+          </div>
         </div>
       </div>
     </div>
