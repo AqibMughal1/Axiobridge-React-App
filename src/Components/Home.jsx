5929ec1c201a782f4ef8d61f1e769f2d0abb4eb6
AQIBMUGHAL1 <fa21-bcs-073@cuilahore.edu.pk>
2023-09-09 00:21:57 +0500

    🚑

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,15 +1,25 @@
 import React from 'react'
 import Navbar from './Navbar'
 import Hero from './Hero'
-
+import WhyUs from './WhyUs'
+import Threecards from './Threecards'
+import Ourclients from './Ourclients'
+import Toolcard from './Toolcard'
+import Services from './Services'
 
 const Home = () => {
   return (
     <div>
         <Navbar/>
         <Hero/>
-    </div>
-  );
-};
+        <WhyUs/>
+        <Threecards/>
+        <Toolcard/>
+        <Services/>
+        <Ourclients/>
 
-export default Home;
+    </div>
+  )
+}
+
+export default Home
