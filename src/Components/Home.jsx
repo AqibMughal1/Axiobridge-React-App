995525f1cb0e4fd7dad79475019040e531940af8
ahmed <ahmadabdullah4919@gmail.com>
2023-09-10 18:59:54 +0500

    add faq

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from "react";
 import Navbar from "./Navbar";
 import Hero from "./Hero";
@@ -7,23 +6,12 @@ import Ourclients from "./Ourclients";
 import Toolcard from "./Toolcard";
 import Services from "./Services";
 import Whyus from "./Whyus";
+import Footer from "./Footer";
 import FAQ from "./FAQ";
-=======
-import React from 'react'
-import Navbar from './Navbar'
-import Hero from './Hero'
-import Threecards from './Threecards'
-import Ourclients from './Ourclients'
-import Toolcard from './Toolcard'
-import Services from './Services'
-import Whyus from './Whyus'
-import Footer from './Footer'
->>>>>>> 34d24f9f3e3f38e95ffa5c7b2dba5c5eea89fad2
 
 const Home = () => {
   return (
     <div>
-<<<<<<< HEAD
       <Navbar />
       <Hero />
       <Whyus />
@@ -32,18 +20,7 @@ const Home = () => {
       <Services />
       <Ourclients />
       <FAQ />
-=======
-        <Navbar/>
-        <Hero/>
-        <Whyus/>
-        <Threecards/>
-        <Toolcard/>
-        <Services/>
-        <Ourclients/>
-        <Footer/>
-
-
->>>>>>> 34d24f9f3e3f38e95ffa5c7b2dba5c5eea89fad2
+      <Footer />
     </div>
   );
 };
